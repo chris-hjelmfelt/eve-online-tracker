b20900f3484b1163f0bf4547772317fbc6acbca5
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2021-07-30 15:32:18 -0600

    Vue client added

---
+++ README.md
@@ -0,0 +1,10 @@
+## Eve Online Tracker   
+
+Uses the ESI Evetech API to get info about characters, alliances, and regions   
+
+Made with Node, Express, and Vue   
+
+**Run the project**   
+```
+> npm run dev   
+```
---
+++ client/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <img :src="image" alt="eve online logo">
+  </header>
+
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data() {
+      return {
+        image: require('@/assets/eve-logo.png')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  img {
+    width: 400px;
+  }
+  @media(max-width: 700px){
+    img {
+      display: block;
+      margin: auto;
+    }
+  }
+</style>
