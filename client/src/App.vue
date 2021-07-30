b1734608826818c5f332ba0c2f280cd5d27bbf3f
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2021-07-30 16:28:24 -0600

    added search component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="container">
     <Header />
+    <Search />
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Search from './components/Search.vue';
+
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Search
   }
 }
 </script>
---
+++ client/src/components/Search.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="search">
+    <h2>Search Player Stats</h2>
+    <form v-on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="platform">Search Type</label>
+        <select name="platform" id="platform" v-model="platform">
+          <option value="characters">Character</option>
+          <option value="alliances">Alliance</option>
+          <option value="regions">Region</option>          
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="playerid">Player ID</label>
+        <input type="text" name="text" id="playerid" v-model="playerID" />
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Submit" class="btn">
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "Search",
+    data() {
+      return{
+        platform: "",
+        playerID: ""
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
